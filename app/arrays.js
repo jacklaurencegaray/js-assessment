d13edbc93c6a17b285b56487f56b4f7e343282b2
jacklaurencegaray <lawrencetecho@gmail.com>
2019-08-02 03:29:23 +0800

    Answers on array

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,113 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
-
+  indexOf: function (arr, item) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
+    return -1;
   },
 
-  sum: function(arr) {
-
+  sum: function (arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
-  remove: function(arr, item) {
+  remove: function (arr, item) {
+    var newArr = arr.slice();
 
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        newArr.splice(i, 1);
+        i--;
+      }
+    }
+    return newArr;
   },
 
-  removeWithoutCopy: function(arr, item) {
-
+  removeWithoutCopy: function (arr, item) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+    return arr;
   },
 
-  append: function(arr, item) {
-
+  append: function (arr, item) {
+    arr[arr.length] = item;
+    return arr;
   },
 
-  truncate: function(arr) {
-
+  truncate: function (arr) {
+    arr.length--;
+    return arr;
   },
 
-  prepend: function(arr, item) {
-
+  prepend: function (arr, item) {
+    arr.unshift(item);
+    return arr;
   },
 
-  curtail: function(arr) {
-
+  curtail: function (arr) {
+    arr.shift();
+    return arr;
   },
 
-  concat: function(arr1, arr2) {
-
+  concat: function (arr1, arr2) {
+    return arr1.concat(arr2);
   },
 
-  insert: function(arr, item, index) {
-
+  insert: function (arr, item, index) {
+    arr.splice(index, 0, item);
+    return arr;
   },
 
-  count: function(arr, item) {
-
+  count: function (arr, item) {
+    var count = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        count++;
+      }
+    }
+    return count;
   },
 
-  duplicates: function(arr) {
+  duplicates: function (arr) {
+    var numbers = {};
+    var duplicates = [];
 
+    for (var i = 0; i < arr.length; i++) {
+      if (numbers[arr[i]]) {
+        duplicates.push(arr[i]);
+      } else {
+        numbers[arr[i]] = true;
+      }
+    }
+
+    return duplicates;
   },
 
-  square: function(arr) {
-
+  square: function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] *= arr[i];
+    }
+    return arr;
   },
 
-  findAllOccurrences: function(arr, target) {
-
+  findAllOccurrences: function (arr, target) {
+    var occurrences = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        occurrences.push(i);
+      }
+    }
+    return occurrences;
   }
 };
